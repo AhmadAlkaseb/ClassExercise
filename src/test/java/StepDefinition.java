5ef4f30d787bb28ef6be49a2ac2709d83ad66343
AhmadAlkaseb <ahmadalkasebpri@icloud.com>
2024-09-24 14:31:21 +0200

    .

--- src/test/java/StepDefinition.java
+++ src/test/java/StepDefinition.java
@@ -6,16 +6,18 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StepDefinition {
-    public static ArrayList<Integer> numbers = new ArrayList<>();
-    public static int result = 0;
+    public static ArrayList<Double> numbers = new ArrayList<>();
+    public static double result = 0;
 
-    @Given("I have the number {int}")
-    public void iHaveTheNumber(int arg0) {
+    @Given("I have the number {double}")
+    public void iHaveTheNumber(double arg0) {
         numbers.add(arg0);
     }
 
     @Then("I want to see the result {int} in my display.")
-    public void iWantToSeeTheResultInMyDisplay(int arg0) {
+    public void iWantToSeeTheResultInMyDisplay(double arg0) {
         assertEquals(arg0, result);
+        numbers.clear();
     }
+
 }
