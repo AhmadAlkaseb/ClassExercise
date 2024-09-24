12bf1ba52b48b338a9f1f4b420d9b94c4bed70c5
lassekh <31202809+lassekh@users.noreply.github.com>
2024-09-24 14:48:51 +0200

    Percentage

--- src/main/java/CalculatorController.java
+++ src/main/java/CalculatorController.java
@@ -2,19 +2,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CalculatorController {
-    public int add(List<Integer> numbers) {
-        int result=0;
-        for (Integer number : numbers) {
+    public double add(List<Double> numbers) {
+        double result = 0;
+        for (double number : numbers) {
             result += number;
         }
         return result;
     }
 
-    public int modulus(ArrayList<Integer> numbersForModulus) {
+    public double modulus(ArrayList<Double> numbersForModulus) {
         return numbersForModulus.get(0) % numbersForModulus.get(1);
     }
 
-    public int subtract(Integer integer, Integer integer1) {
+    public double subtract(double a, double b) {
         return 0;
+    }
+
+    public double percentageOf(double a, double b) {
+        return a/100*b;
+    }
+
+    public double getPercent(Double a, Double b) {
+        return a/b*100;
     }
 }
--- src/test/java/PercentageCalculation.feature
+++ src/test/java/PercentageCalculation.feature
@@ -1,10 +1,16 @@
-Feature: Percentage of a number
+Feature: Percentage Calculation
   As a user
   I want to calculate a percentage of a number
   So that i can see the result
 
   Scenario: Percentage of a number
-    Given I have entered 25
-    And I have entered 769
+    Given I have the number 25
+    And I have the number 769
     When I click Calculate Percentage
-    Then I want to see the result 192.25 in my display
+    Then I want to see the result 192.25 in my display.
+
+  Scenario: How many percent are A of B
+    Given I have the number 7
+    And I have the number 28
+    When I click Get Percent
+    Then I want to see the result 25 in my display.
